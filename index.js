317bee5b526f3ebf4569b334cfaeb203751d3f7c
CodeTrove Development <47091176+codetrove-development@users.noreply.github.com>
2019-02-16 20:27:40 +0000

    renamed snappy-grid as name in use

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const options = require('./build/options')
 
-module.exports = require('./build/snappy-grid').default
+module.exports = require('./build/curator-core').default
 module.exports.helpers = require('./build/helpers').default
 module.exports.defaultGridOptions = options.defaultGridOptions
 module.exports.defaultItemOptions = options.defaultItemOptions
