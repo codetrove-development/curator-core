1698c34bae5a6d2ce423a767d37b9bc85f25aff6
CodeTrove Development <47091176+codetrove-development@users.noreply.github.com>
2019-02-16 20:28:42 +0000

    added objIsInArray

--- src/helpers.js
+++ src/helpers.js
@@ -2,6 +2,13 @@ const isDefined = ( item ) => {
     return typeof item !== 'undefined' && item != null
 }
 
+const objIsInArray = ( array, obj, key ) => {
+    const objKeyValue = obj[ key ]
+
+    return array.find(item => item[ key ] === objKeyValue) != null
+}
+
 export default {
     isDefined,
+    objIsInArray,
 }
